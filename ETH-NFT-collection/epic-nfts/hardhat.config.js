c28e49283bc326af02b53966e0db46f1b3c2504f
harada <harada@fivit.co.jp>
2022-08-10 16:46:25 +0900

    epic-nfts

--- ETH-NFT-collection/epic-nfts/hardhat.config.js
+++ ETH-NFT-collection/epic-nfts/hardhat.config.js
@@ -3,4 +3,10 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    rinkeby: {
+      url: "https://eth-rinkeby.alchemyapi.io/v2/Ah5tyekDMDmhnWGqFq4HnNipeScLdIOo",
+      accounts: ["c6ab7688a2dc18ad8a37925ed97dd729a337ea279116874ee30785ae20b37d23"],
+    },
+  },
 };
